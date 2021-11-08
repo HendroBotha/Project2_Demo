ef2808758c1dd2d5b3714d1b302d65ce5df3a381
Hendro Botha <hendrobotha08@gmail.com>
2021-11-08 12:00:46 +0200

    Added database properties to class and data annotations.

--- Project2_Demo/Models/Category.cs
+++ Project2_Demo/Models/Category.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Project2_Demo.Models
 {
     public class Category
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public String Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime DateTimeCreated { get; set; } = DateTime.Now;
+
     }
 }
